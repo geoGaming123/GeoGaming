e686a3e77ecf93508a0bdea53fdefa1b9e77c166
GiThibs <adelethibaut@gmail.com>
2024-02-15 09:38:41 +0100

    remove define props from imports

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script setup>
-import { defineProps, computed } from 'vue'
+import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const props = defineProps({
