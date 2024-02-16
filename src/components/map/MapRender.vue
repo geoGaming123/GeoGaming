a3dda965a3c6fa6c72eaa07e3306883707e82fbf
FlorianAuc <florian.aucouturier@gmail.com>
2024-02-16 09:35:24 +0100

    add position dynamique user

--- src/components/map/MapRender.vue
+++ src/components/map/MapRender.vue
@@ -1,58 +1,107 @@
 <template>
-    <div class="container">
-      <h2>{{ gamesStore.formData.title }}</h2>
-      <p>{{ gamesStore.formData.description }}</p>
-      <p>Start Date: {{ gamesStore.formData.startDate }}</p>
-      <p>End Date: {{ gamesStore.formData.endDate }}</p>
-      <div id="map"></div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted } from 'vue';
-  import * as L from 'leaflet';
-  import { useGamesStore } from '@/stores/games';
-  
-  const gamesStore = useGamesStore();
-  
-  onMounted(() => {
-    // Charger les données depuis le localStorage de manière asynchrone
-    gamesStore.loadFromLocalStorage();
-  
-    // Utiliser les coordonnées du startPoint comme centre initial de la carte
-    if (gamesStore.startPoint) {
-      const { latitude, longitude } = gamesStore.startPoint.position;
-  
-      // Initialiser une nouvelle carte sans utiliser les props
-      const map = L.map('map', {
-        center: [latitude, longitude],
-        zoom: 15,
-        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
-      });
-  
-      // Ajouter des marqueurs pour les données stockées une fois que la carte est prête
-      map.whenReady(() => {
-        gamesStore.markers.forEach(marker => {
-          const { latitude, longitude } = marker.position;
-          const markerIcon = L.icon({
-            iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
-            iconSize: [50, 50],
-            iconAnchor: [12, 41],
-            popupAnchor: [0, -30],
-          });
-          L.marker([latitude, longitude], { icon: markerIcon }).addTo(map).bindPopup(`<b>${marker.name}</b>`);
-        });
-  
-        // Ajouter un marqueur pour le startPoint
-        const startPointIcon = L.icon({
-          iconUrl: 'https://static.thenounproject.com/png/4418877-200.png',
+  <div class="container">
+    <h2>{{ gamesStore.formData.title }}</h2>
+    <p>{{ gamesStore.formData.description }}</p>
+    <p>Start Date: {{ gamesStore.formData.startDate }}</p>
+    <p>End Date: {{ gamesStore.formData.endDate }}</p>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as L from 'leaflet'
+import { useGamesStore } from '@/stores/games'
+
+const gamesStore = useGamesStore()
+
+onMounted(() => {
+  // Charger les données depuis le localStorage de manière asynchrone
+  gamesStore.loadFromLocalStorage()
+
+  // Utiliser les coordonnées du startPoint comme centre initial de la carte
+  if (gamesStore.startPoint) {
+    const { latitude, longitude } = gamesStore.startPoint.position
+
+    // Initialiser une nouvelle carte sans utiliser les props
+    const map = L.map('map', {
+      center: [latitude, longitude],
+      zoom: 15,
+      layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
+    })
+
+    // Ajouter des marqueurs pour les données stockées une fois que la carte est prête
+    map.whenReady(() => {
+      gamesStore.markers.forEach((marker) => {
+        const { latitude, longitude } = marker.position
+        const markerIcon = L.icon({
+          iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
           iconSize: [50, 50],
           iconAnchor: [12, 41],
-          popupAnchor: [0, -30],
-        });
-        L.marker([latitude, longitude], { icon: startPointIcon }).addTo(map).bindPopup('<b>Start Point</b>');
-      });
-    }
-  });
-  </script>
-  
+          popupAnchor: [0, -30]
+        })
+        L.marker([latitude, longitude], { icon: markerIcon })
+          .addTo(map)
+          .bindPopup(`<b>${marker.name}</b>`)
+      })
+
+      // Ajouter un marqueur pour le startPoint
+      const startPointIcon = L.icon({
+        iconUrl: 'https://static.thenounproject.com/png/4418877-200.png',
+        iconSize: [50, 50],
+        iconAnchor: [12, 41],
+        popupAnchor: [0, -30]
+      })
+      L.marker([latitude, longitude], { icon: startPointIcon })
+        .addTo(map)
+        .bindPopup('<b>Start Point</b>')
+
+      // Obtenir la position de l'utilisateur
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords
+            // Ajouter un marqueur pour la position de l'utilisateur
+            const userMarker = L.marker([latitude, longitude])
+              .addTo(map)
+              .bindPopup('Vous êtes ici!')
+              .openPopup()
+            // Stocker la référence du marqueur de l'utilisateur dans le store
+            gamesStore.userMarker = userMarker
+          },
+          (error) => {
+            console.error('Erreur de géolocalisation :', error.message)
+          }
+        )
+
+        // Ajouter la fonctionnalité de suivi en temps réel de la position de l'utilisateur
+        const watchUserPosition = navigator.geolocation.watchPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords
+            // Mettre à jour la position du joueur dans le store
+            gamesStore.updateUserPosition({ latitude, longitude })
+            // Vérifiez si le marqueur de l'utilisateur existe déjà
+            if (gamesStore.userMarker) {
+              gamesStore.userMarker.setLatLng([latitude, longitude])
+            } else {
+              gamesStore.userMarker = L.marker([latitude, longitude])
+                .addTo(map)
+                .bindPopup('Vous êtes ici!')
+                .openPopup()
+            }
+          },
+          (error) => {
+            console.error('Erreur de géolocalisation :', error.message)
+          },
+          {
+            enableHighAccuracy: true, // Activer une haute précision de la géolocalisation
+            maximumAge: 0 // Ne pas utiliser de position mise en cache
+          }
+        )
+      } else {
+        console.error("La géolocalisation n'est pas prise en charge par ce navigateur.")
+      }
+    })
+  }
+})
+</script>
--- src/stores/games.js
+++ src/stores/games.js
@@ -1,6 +1,6 @@
 // stores/games.js
-import { defineStore } from 'pinia';
-import { reactive } from 'vue';
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 
 export const useGamesStore = defineStore('games', {
   state: () => ({
@@ -8,22 +8,25 @@ export const useGamesStore = defineStore('games', {
       title: '',
       description: '',
       startDate: '',
-      endDate: '',
+      endDate: ''
     },
     markers: reactive([]),
-    startPoint: reactive(null), 
+    startPoint: reactive(null),
+    userPosition: reactive({}),
+    userMarker: null
   }),
 
   actions: {
     loadFromLocalStorage() {
-      const storedData = localStorage.getItem('formData');
+      const storedData = localStorage.getItem('formData')
 
       if (storedData) {
-        const parsedData = JSON.parse(storedData);
+        const parsedData = JSON.parse(storedData)
 
-        this.formData = parsedData.formData || this.formData;
-        this.markers = reactive(parsedData.markers || []);
-        this.startPoint = reactive(parsedData.startPoint || null);
+        this.formData = parsedData.formData || this.formData
+        this.markers = reactive(parsedData.markers || [])
+        this.startPoint = reactive(parsedData.startPoint || null)
+        this.userPosition = reactive(parsedData.userPosition || null)
       }
     },
 
@@ -32,22 +35,23 @@ export const useGamesStore = defineStore('games', {
         formData: { ...this.formData },
         markers: this.markers.map((marker) => ({
           name: marker.name,
-          position: { ...marker.position },
+          position: { ...marker.position }
         })),
         startPoint: this.startPoint
           ? {
               name: this.startPoint.name,
-              position: { ...this.startPoint.position },
+              position: { ...this.startPoint.position }
             }
           : null,
-      };
+        userPosition: this.userPosition
+      }
 
-      localStorage.setItem('formData', JSON.stringify(cleanData));
-      console.log('Envoi au localStorage');
+      localStorage.setItem('formData', JSON.stringify(cleanData))
+      console.log('Envoi au localStorage')
     },
 
     updateStartPoint(startPoint) {
-      this.startPoint = startPoint;
+      this.startPoint = startPoint
     },
 
     updateMarkers(markers) {
@@ -55,23 +59,34 @@ export const useGamesStore = defineStore('games', {
         markers.map((marker) => ({
           name: marker.name,
           marker: marker.marker,
-          position: { ...marker.position },
+          position: { ...marker.position }
         }))
-      );
+      )
     },
 
     deleteMarker(marker) {
-      this.markers = this.markers.filter((m) => m.name !== marker.name);
+      this.markers = this.markers.filter((m) => m.name !== marker.name)
       // Mettez à jour le store après la suppression du marqueur
-      this.updateMarkers(this.markers);
-      console.log("supprimer du store");
+      this.updateMarkers(this.markers)
+      console.log('supprimer du store')
     },
 
     deleteStartPoint() {
-      this.startPoint = null;
-      console.log("supprimer du store");
+      this.startPoint = null
+      console.log('supprimer du store')
       this.updateStartPoint(null)
     },
-  },
-});
 
+    updateUserPosition(position) {
+      // Mettre à jour la position du joueur dans le store
+      this.userPosition = position
+      // Sauvegarder la position du joueur dans le localStorage
+      this.saveDataToLocalStorage()
+    },
+
+    updateUserMarker(marker) {
+      // Mettre à jour la référence du marqueur de l'utilisateur dans le store
+      this.userMarker = marker
+    }
+  }
+})
