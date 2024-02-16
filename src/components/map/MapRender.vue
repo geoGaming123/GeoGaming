e6664f9dc2b8ee4ded9038f1cbd78bf62ca24e4c
Corentinmiserque <corentinmiserque1@gmail.com>
2024-02-16 13:37:56 +0100

    add api

--- src/components/map/MapRender.vue
+++ src/components/map/MapRender.vue
@@ -1,58 +1,64 @@
 <template>
-    <div class="container">
-      <h2>{{ gamesStore.formData.title }}</h2>
-      <p>{{ gamesStore.formData.description }}</p>
-      <p>Start Date: {{ gamesStore.formData.startDate }}</p>
-      <p>End Date: {{ gamesStore.formData.endDate }}</p>
-      <div id="map"></div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted } from 'vue';
-  import * as L from 'leaflet';
-  import { useGamesStore } from '@/stores/games';
-  
-  const gamesStore = useGamesStore();
-  
-  onMounted(() => {
-    // Charger les données depuis le localStorage de manière asynchrone
-    gamesStore.loadFromLocalStorage();
-  
-    // Utiliser les coordonnées du startPoint comme centre initial de la carte
-    if (gamesStore.startPoint) {
-      const { latitude, longitude } = gamesStore.startPoint.position;
-  
+  <div class="container">
+    <h2>{{ gamesStore.title }}</h2>
+    <p>{{ gamesStore.description }}</p>
+    <p>Start Date: {{ gamesStore.startDate }}</p>
+    <p>End Date: {{ gamesStore.endDate }}</p>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as L from 'leaflet'
+import { useGamesStore } from '@/stores/games'
+
+const gamesStore = useGamesStore()
+const id = 325;
+
+onMounted(() => {
+  gamesStore.loadFromLocalStorage()
+
+  // Utiliser les coordonnées du start_point comme centre initial de la carte
+  if (gamesStore.start_point && gamesStore.start_point.position) {
+    const { latitude, longitude } = gamesStore.start_point.position
+
+    // Vérifier si les coordonnées sont valides
+    if (!isNaN(latitude) && !isNaN(longitude)) {
       // Initialiser une nouvelle carte sans utiliser les props
       const map = L.map('map', {
         center: [latitude, longitude],
         zoom: 15,
-        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
-      });
-  
+        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
+      })
+
       // Ajouter des marqueurs pour les données stockées une fois que la carte est prête
       map.whenReady(() => {
-        gamesStore.markers.forEach(marker => {
-          const { latitude, longitude } = marker.position;
+        gamesStore.markers.forEach((marker) => {
+          const { latitude, longitude } = marker.position
           const markerIcon = L.icon({
             iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
             iconSize: [50, 50],
             iconAnchor: [12, 41],
-            popupAnchor: [0, -30],
-          });
-          L.marker([latitude, longitude], { icon: markerIcon }).addTo(map).bindPopup(`<b>${marker.name}</b>`);
-        });
-  
-        // Ajouter un marqueur pour le startPoint
+            popupAnchor: [0, -30]
+          })
+          L.marker([latitude, longitude], { icon: markerIcon })
+            .addTo(map)
+            .bindPopup(`<b>${marker.name}</b>`)
+        })
+
+        // Ajouter un marqueur pour le start_point
         const startPointIcon = L.icon({
           iconUrl: 'https://static.thenounproject.com/png/4418877-200.png',
           iconSize: [50, 50],
           iconAnchor: [12, 41],
-          popupAnchor: [0, -30],
-        });
-        L.marker([latitude, longitude], { icon: startPointIcon }).addTo(map).bindPopup('<b>Start Point</b>');
-      });
+          popupAnchor: [0, -30]
+        })
+        L.marker([latitude, longitude], { icon: startPointIcon })
+          .addTo(map)
+          .bindPopup('<b>Start Point</b>')
+      })
     }
-  });
-  </script>
-  
+  }
+})
+</script>
--- src/stores/games.js
+++ src/stores/games.js
@@ -1,6 +1,6 @@
 // stores/games.js
-import { defineStore } from 'pinia';
-import { reactive } from 'vue';
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
 
 export const useGamesStore = defineStore('games', {
   state: () => ({
@@ -8,70 +8,196 @@ export const useGamesStore = defineStore('games', {
       title: '',
       description: '',
       startDate: '',
-      endDate: '',
+      endDate: ''
     },
     markers: reactive([]),
-    startPoint: reactive(null), 
+    startPoint: reactive(null),
+    userPosition: reactive({}),
+    userMarker: null
   }),
 
   actions: {
-    loadFromLocalStorage() {
-      const storedData = localStorage.getItem('formData');
 
-      if (storedData) {
-        const parsedData = JSON.parse(storedData);
 
-        this.formData = parsedData.formData || this.formData;
-        this.markers = reactive(parsedData.markers || []);
-        this.startPoint = reactive(parsedData.startPoint || null);
-      }
+    // loadFromLocalStorage() {
+    //   const storedData = localStorage.getItem('testData')
+
+    //   if (storedData) {
+    //     const parsedData = JSON.parse(storedData)
+
+    //     this.formData = parsedData.formData || this.formData
+    //     this.markers = reactive(parsedData.markers || [])
+    //     this.startPoint = reactive(parsedData.startPoint || null)
+    //     this.userPosition = reactive(parsedData.userPosition || null)
+    //   }
+    // },
+
+loadFromLocalStorage() {
+  const storedData = localStorage.getItem('testData');
+
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+
+    this.title = parsedData.title || this.title;
+    this.description = parsedData.description || this.description;
+    this.startDate = parsedData.startDate || this.startDate;
+    this.endDate = parsedData.endDate || this.endDate;
+    this.markers = reactive(parsedData.markers || []);
+    this.start_point = reactive(parsedData.start_point || null);
+    this.userPosition = reactive(parsedData.userPosition || null);
+  }
+},
+
+
+
+getMatches() {
+  const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NlcGVncmEtZnJvbnRlbmQueHl6L3dmMTEtYXRlbGllciIsImlhdCI6MTcwNzk5MDE5NSwibmJmIjoxNzA3OTkwMTk1LCJleHAiOjE3MDg1OTQ5OTUsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.fgYfqHYmhNdFnW0xOoL2pY1HBsBCgThfi-6sy2ti-FQ";
+
+  // Fetch matches data from the API
+  fetch('https://cepegra-frontend.xyz/wf11-atelier/wp-json/wp/v2/match', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
     },
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Traitez les données récupérées
+      state.title.value = data.title || state.title.value;
+      state.description.value = data.description || state.description.value;
+      state.startDate.value = data.startDate || state.startDate.value;
+      state.endDate.value = data.endDate || state.endDate.value;
+      state.markers.value = reactive(data.markers || []);
+      state.start_point.value = reactive(data.start_point || null);
+      state.userPosition.value = reactive(data.userPosition || null);
+      state.matches.value = reactive(data);
 
-    saveDataToLocalStorage() {
-      const cleanData = {
-        formData: { ...this.formData },
-        markers: this.markers.map((marker) => ({
-          name: marker.name,
-          position: { ...marker.position },
-        })),
-        startPoint: this.startPoint
-          ? {
-              name: this.startPoint.name,
-              position: { ...this.startPoint.position },
-            }
-          : null,
+      console.log('Matches retrieved from API:', state.matches.value);
+    })
+    .catch(error => {
+      console.error('Error fetching matches from API:', error);
+    });
+},
+
+
+    
+    postMatchData() {
+      const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2NlcGVncmEtZnJvbnRlbmQueHl6L3dmMTEtYXRlbGllciIsImlhdCI6MTcwNzk5MDE5NSwibmJmIjoxNzA3OTkwMTk1LCJleHAiOjE3MDg1OTQ5OTUsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.fgYfqHYmhNdFnW0xOoL2pY1HBsBCgThfi-6sy2ti-FQ";
+    
+      const matchData = {
+        status: "publish",
+        title: this.formData.title,
+        fields: {
+          title: this.formData.title,
+          description: this.formData.description,
+          start_date: this.formData.startDate,
+          end_date: this.formData.endDate,
+          markers: this.markers.map((marker) => ({
+            name: marker.name,
+            position: { ...marker.position },
+            penality: "20",
+          })),
+          start_point: this.startPoint
+            ? {
+                name: this.startPoint.name,
+                position: { ...this.startPoint.position },
+              }
+            : null,
+          masteruid: '1', // Modifié en brut
+          players: [
+            {
+              userId: '2',
+              time: '10',
+              markers: [
+                {
+                  marker_id: '1',
+                  check: 'false',
+                },
+                {
+                  marker_id: '3',
+                  check: 'false',
+                },
+              ],
+            },
+            {
+              userId: '3',
+              time: '10',
+              markers: [
+                {
+                  marker_id: '1',
+                  check: 'false',
+                },
+                {
+                  marker_id: '3',
+                  check: 'false',
+                },
+              ],
+            },
+          ],
+        },
       };
-
-      localStorage.setItem('formData', JSON.stringify(cleanData));
-      console.log('Envoi au localStorage');
+    
+      fetch('https://cepegra-frontend.xyz/wf11-atelier/wp-json/wp/v2/match', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(matchData),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Réponse de l\'API :', data);
+          // Ajoutez ici toute logique de gestion de la réponse de l'API
+        })
+        .catch(error => {
+          console.error('Erreur lors de la requête POST :', error);
+          // Ajoutez ici toute logique de gestion des erreurs
+        });
     },
+    
 
-    updateStartPoint(startPoint) {
-      this.startPoint = startPoint;
-    },
+
 
     updateMarkers(markers) {
       this.markers = reactive(
         markers.map((marker) => ({
           name: marker.name,
           marker: marker.marker,
-          position: { ...marker.position },
+          position: { ...marker.position }
         }))
-      );
+      )
+    },
+
+    updateStartPoint(startPoint) {
+      this.startPoint = startPoint
     },
 
     deleteMarker(marker) {
-      this.markers = this.markers.filter((m) => m.name !== marker.name);
+      this.markers = this.markers.filter((m) => m.name !== marker.name)
       // Mettez à jour le store après la suppression du marqueur
-      this.updateMarkers(this.markers);
-      console.log("supprimer du store");
+      this.updateMarkers(this.markers)
+      console.log('supprimer du store')
     },
 
     deleteStartPoint() {
-      this.startPoint = null;
-      console.log("supprimer du store");
-      updateStartPoint()
+      this.startPoint = null
+      console.log('supprimer du store')
+      this.updateStartPoint(null)
     },
-  },
-});
+
+    updateUserPosition(position) {
+      // Mettre à jour la position du joueur dans le store
+      this.userPosition = position
+      // Sauvegarder la position du joueur dans le localStorage
+      this.saveDataToLocalStorage()
+    },
+
+    updateUserMarker(marker) {
+      // Mettre à jour la référence du marqueur de l'utilisateur dans le store
+      this.userMarker = marker
+    }
+  }
+})
 
