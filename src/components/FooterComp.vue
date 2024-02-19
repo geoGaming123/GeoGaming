186b733324acdc720120b5623c972b1087cdeebb
GiThibs <adelethibaut@gmail.com>
2024-02-19 15:11:23 +0100

    moved footer css to scss

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="footer">
-    <button @click="activateTab('past')" :class="{ activebtn: activeTab === 'past' }">Finies</button>
-    <button @click="activateTab('present')" :class="{ activebtn: activeTab === 'present' }">En cours</button>
-    <button @click="activateTab('futur')" :class="{ activebtn: activeTab === 'futur' }">Futures</button>
+    <button @click="activateTab('past')" :class="{ footer_activebtn: activeTab === 'past' }">Finies</button>
+    <button @click="activateTab('present')" :class="{ footer_activebtn: activeTab === 'present' }">En cours</button>
+    <button @click="activateTab('futur')" :class="{ footer_activebtn: activeTab === 'futur' }">Futures</button>
   </section>
 </template>
 
@@ -19,30 +19,5 @@ const activateTab = (tab) => {
 </script>
 
 <style scoped>
-.footer {
-  width: 100%;
-  height: 60px;
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  display: flex;
-  justify-content: space-evenly;
-  background-color: #f1f1f1;
-}
-button {
-  width: 33%;
-  border: 1px solid #f1f1f1;
-  background-color: #f1f1f1;
-  color: black;
-  padding: 1rem;
-  font-weight: bold;
-  font-size: 1rem;
-}
-.activebtn {
-  background-color: orange;
-  color: white;
-  border: 1px solid orange;
-}
-
 
 </style>
