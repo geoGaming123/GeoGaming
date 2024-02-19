397494c323ab1370b6513dd982d56d843acc5fb4
GiThibs <adelethibaut@gmail.com>
2024-02-19 11:46:48 +0100

    Add reactive homeView for temporality

--- src/components/GameListItem.vue
+++ src/components/GameListItem.vue
@@ -1,39 +1,22 @@
 <template>
-  <tbody>
-    <tr class="gamelist-item">
-      <th>{{ props.data }}</th>
-      <th>{{ props.data }}</th>
-      <th>{{ props.data }}</th>
-      <th>{{ props.data }}</th>
-    </tr>
-    <tr class="gamelist-item">
-      <th>1</th>
-      <th>Charleroi</th>
-      <th>9</th>
-      <th>10/03/24</th>
-    </tr>
-    <tr class="gamelist-item">
-      <th>2</th>
-      <th>Bruxelles</th>
-      <th>33</th>
-      <th>15/03/24</th>
-    </tr>
-    <tr class="gamelist-item">
-      <th>3</th>
-      <th>Paris</th>
-      <th>3</th>
-      <th>22/03/24</th>
-    </tr>
-  </tbody>
+  <tr class="gamelist-item">
+    <th>{{ props.index + 1 }}</th>
+    <th>{{ props.aMatch.acf.title }}</th>
+    <th>{{ JSON.parse(props.aMatch.acf.players).length }}</th>
+    <th>{{ formatDate(props.aMatch.acf.start_date) }}</th>
+  </tr>
 </template>
 
 <script setup>
 
  const props = defineProps({
-    data : String
+    aMatch : Object,
+    index: Number
   })
-
-
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('fr-FR'); // Changez 'fr-FR' par le code de la locale souhaitée
+};
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
     <HeaderComp data="logo" profile="profile"></HeaderComp>
-    <GameList :menu="showMenuData"></GameList>
+    <GameList :MatchesData="PastMatchesData" v-show="showMenuData == 'past'" :menu="showMenuData"></GameList>
+    <GameList :MatchesData="PresentMatchesData" v-show="showMenuData == 'present'" :menu="showMenuData"></GameList>
+    <GameList :MatchesData="FutureMatchesData" v-show="showMenuData == 'futur'" :menu="showMenuData"></GameList>
     <FooterComp @activate-menu="showMenu"></FooterComp>
   </main>
 </template>
@@ -11,12 +13,25 @@
 import HeaderComp from '@/components/HeaderComp.vue';
 import FooterComp from '@/components/FooterComp.vue';
 import GameList from '@/components/GameList.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useCounterStore } from '@/stores/counter'
+
+const monStore = useCounterStore()
+monStore.getAllMatches(); // Charge les données des parties et distribue en 3 array selon la temporalité
 
 const showMenuData = ref('present')
 const showMenu = (tab) => {
   showMenuData.value = tab
 }
+const FutureMatchesData = computed(() => { // Récupère le tableau des parties futures
+  return monStore.getMatchesFuture
+})
+const PastMatchesData = computed(() => { // Récupère le tableau des parties passées
+  return monStore.getMatchesPast
+})
+const PresentMatchesData = computed(() => { // Récupère le tableau des parties en cours
+  return monStore.getMatchesPresent
+})
 
 </script>
 
