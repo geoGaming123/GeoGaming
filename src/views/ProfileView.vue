f8b5b8fa74679d4aec36eb4cd51b512439cdc84b
GiThibs <adelethibaut@gmail.com>
2024-02-19 15:46:17 +0100

    Moved profile css to scss

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -7,30 +7,30 @@ import HeaderComp from '@/components/HeaderComp.vue';
 <template>
   <main>
     <HeaderComp></HeaderComp>
-    <div class="profileView">
-      <section class="profileInfo">
+    <div class="profile">
+      <section class="profile_Info">
         <h1>Edition du profil</h1>
         <p>Modifier votre avatar ou votre nom d'utilisateur</p>
       </section>
       <hr>
-      <div class="form">
+      <div class="profile_form">
         <form>
-          <section class="profilePicture">
-            <div class="profilePictureLeft">
+          <section class="profile_form_Picture">
+            <div class="profile_form_Picture_Left">
               <h2>Photo de profil</h2>
               <p>(Recommandé 300x300)</p>
               <button>Changer</button>
             </div>
-            <div class="profilePictureImg">
+            <div class="profile_form_Picture_Img">
               <img src="" alt="Ma photo">
             </div>
           </section>
-          <section class="profilePseudo">
+          <section class="profile_form_Pseudo">
             <label for="pseudo">Pseudo</label><br>
             <input id="pseudo" type="text"> 
           </section>
-          <section class="profileSave">
-            <button class="save">Sauvegarder</button>
+          <section class="profile_form_Save">
+            <button class="profile_form_Save_btn">Sauvegarder</button>
           </section>
         </form>
       </div>
@@ -39,50 +39,5 @@ import HeaderComp from '@/components/HeaderComp.vue';
 </template>
 
 <style scoped>
-
-.profileView {
-  padding-inline: 2rem
-}
-.profileInfo {
-  margin-block: 2rem;
-}
-.form {
-  display: flex;
-  width: 100%;
-}
-form {
-  width: 100%;
-}
-.profilePicture {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  margin-block: 4rem;
-}
-button {
-  padding: 1rem;
-  border: 1px solid orange;
-  background-color: orange;
-  color: white;
-  font-weight: bold;
-  font-size: 1rem;
-  border-radius: 15px;
-}
-#pseudo {
-  width: 50%;
-  padding: 1rem;
-  background-color: #f1f1f153;
-  border: 1px solid #eaeaea;
-  border-radius: 15px;
-}
-.profileSave {
-  width: 100%;
-}
-.save {
-  width: 300px;
-  position: absolute;
-  bottom: 50px;
-  left: calc(50% - 150px) ;
-}
 
 </style>
