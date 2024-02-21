b7490ad921c7ec670e31473c127796be43ec3199
GiThibs <adelethibaut>
2024-02-21 13:18:24 +0100

    Edit to rank view

--- src/components/MyRank.vue
+++ src/components/MyRank.vue
@@ -2,13 +2,15 @@
   <div class="myRank">
     <img src="" alt=" "><span class="myRank_pseudo">Rocko</span>
     <div class="myRank_Stat">
-      <span>15e</span><span>8m 32s</span>
+      <span>15e</span><span>{{ props.rank.time }}</span>
     </div>
   </div>
 </template>
 
 <script setup>
-
+const props = defineProps({
+  rank: Object
+})
 </script>
 
 <style scoped>
