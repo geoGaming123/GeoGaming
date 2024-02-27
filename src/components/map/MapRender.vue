022ded8bdc20ac5a74f9b6e1f1d5cc74ab1c3641
Corentinmiserque <corentinmiserque1@gmail.com>
2024-02-27 15:53:44 +0100

    props

--- src/components/map/MapRender.vue
+++ src/components/map/MapRender.vue
@@ -1,36 +1,36 @@
 <template>
+  <div>
     <h2>{{ match.acf.title }}</h2>
     <p>{{ match.acf.description }}</p>
     <p>Start Date: {{ match.acf.start_date }}</p>
     <p>End Date: {{ match.acf.end_date }}</p>
 
     <div id="map"></div>
-<div v-if="props.timer">
-  <Timer></Timer>
-</div>
+    
+    <div v-if="props.timer">
+      <Timer></Timer>
+    </div>
 
-  <div v-if="props.delete">
-    <ButtonDelete :id="matchId"></ButtonDelete>
+    <div v-if="props.delete">
+      <ButtonDelete :id="matchId"></ButtonDelete>
+    </div>
+
+    <div v-if="props.join">
+      <ButtonJoin :id="matchId"></ButtonJoin>
+    </div>
+
+    <div v-if="props.leave">
+      <ButtonLeaveGame :id="matchId"></ButtonLeaveGame>
+    </div>
+
+    <ButtonModified :id="matchId"></ButtonModified>
   </div>
-<div v-if="props.join">
-  <ButtonJoin :id="matchId"></ButtonJoin>
-</div>
-<div v-if="props.leave">
-<ButtonLeaveGame :id="matchId"></ButtonLeaveGame>
-</div>
-
-<ButtonModified :id="matchId"></ButtonModified>
-
-
 </template>
 
 <script setup>
 import { useGamesStore } from '@/stores/games'
-import { onMounted, computed, ref } from 'vue'
+import { onMounted, computed, ref, watch } from 'vue'
 import * as L from 'leaflet'
-
-
-
 import ButtonDelete from '@/components/map/ButtonDelete.vue'
 import ButtonJoin from './ButtonJoin.vue'
 import ButtonModified from './ButtonModified.vue'
@@ -38,69 +38,80 @@ import ButtonLeaveGame from './ButtonLeaveGame.vue'
 import Timer from '@/components/map/Timer.vue'
 import { userposition } from './Userposition.vue'
 
-
 const gamesStore = useGamesStore()
-const props = defineProps(['id', 'delete', "join", "modified", "startpoint", "markers", "timer", "position", "leave"]);
+const props = defineProps(['id', 'delete', 'join', 'leave', 'startpoint', 'position', 'markers', 'timer'])
 const matchId = props.id
-console.log('matchId' + matchId)
-
 
 gamesStore.getMatch(matchId)
 
-const match = ref(computed(() => {
-  return gamesStore.oneMatch
-}))
- onMounted(()=> {
-  const startPoint = match.value.acf.start_point;
-    const markers = match.value.acf.markers;
-  
-      const latitude = startPoint.position.latitude;
-      const longitude = startPoint.position.longitude;
+const match = ref(
+  computed(() => {
+    return gamesStore.oneMatch
+  })
+)
 
-      const map = L.map('map', {
-        center: [latitude, longitude],
-        zoom: 15,
-        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
-      });
+onMounted(() => {
+  const startPoint = match.value.acf.start_point
+  const markers = match.value.acf.markers
 
-  map.whenReady(() => {
-    if(props.markers){
-      markers.forEach((marker) => {
+  const latitude = startPoint.position.latitude
+  const longitude = startPoint.position.longitude
+
+  const map = L.map('map', {
+    center: [latitude, longitude],
+    zoom: 15,
+    layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
+  })
+
+  const addMarkersToMap = () => {
+    markers.forEach((marker) => {
       const { latitude, longitude } = marker.position
       const markerIcon = L.icon({
         iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
         iconSize: [50, 50],
         iconAnchor: [12, 41],
-        popupAnchor: [0, -30]
+        popupAnchor: [0, -30],
       })
-      const leaflermarker = L.marker([latitude, longitude], { icon: markerIcon })
+      const leafletMarker = L.marker([latitude, longitude], { icon: markerIcon })
         .addTo(map)
         .bindPopup(`<b>${marker.name}</b>`)
 
-      // Assurez-vous que le marqueur a une référence à leafletMarker
-      marker.leafletMarker = leaflermarker
+      marker.leafletMarker = leafletMarker
     })
+  }
+
+  if (props.markers) {
+    addMarkersToMap()
+  }
+
+  watch(() => props.markers, (newValue) => {
+    if (newValue) {
+      addMarkersToMap()
+    } else {
+      markers.forEach((marker) => {
+        map.removeLayer(marker.leafletMarker)
+      })
     }
-   if(props.startpoint){
+  })
+
+  if (props.startpoint) {
     const startPointIcon = L.icon({
       iconUrl: 'https://static.thenounproject.com/png/4418877-200.png',
       iconSize: [50, 50],
       iconAnchor: [12, 41],
-      popupAnchor: [0, -30]
+      popupAnchor: [0, -30],
     })
     L.marker([latitude, longitude], { icon: startPointIcon })
       .addTo(map)
       .bindPopup('<b>Start Point</b>')
-   }
+  }
 
-    
-if(props.position){
-  userposition(map)
-}
-
-  })
+  if (props.position) {
+    userposition(map)
+  }
 })
 </script>
+
 
 
 
