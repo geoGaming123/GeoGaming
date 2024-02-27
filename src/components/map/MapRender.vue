902a8b4ea8aa9f1931c6e36b75a5c43aa61cc85c
FlorianAuc <florian.aucouturier@gmail.com>
2024-02-27 15:18:04 +0100

    update marker

--- src/components/map/MapRender.vue
+++ src/components/map/MapRender.vue
@@ -1,18 +1,15 @@
 <template>
-    <h2>{{ match.acf.title }}</h2>
-    <p>{{ match.acf.description }}</p>
-    <p>Start Date: {{ match.acf.start_date }}</p>
-    <p>End Date: {{ match.acf.end_date }}</p>
+  <h2>{{ match.acf.title }}</h2>
+  <p>{{ match.acf.description }}</p>
+  <p>Start Date: {{ match.acf.start_date }}</p>
+  <p>End Date: {{ match.acf.end_date }}</p>
 
-    <div id="map"></div>
+  <div id="map"></div>
 
-  <Timer></Timer>
+  <Timer :updateShowMarkers="updateShowMarkers"></Timer>
 
-
-    <ButtonDelete :id="matchId"></ButtonDelete>
-    <ButtonJoin :id="matchId"></ButtonJoin>
-
-
+  <ButtonDelete :id="matchId"></ButtonDelete>
+  <ButtonJoin :id="matchId"></ButtonJoin>
 </template>
 
 <script setup>
@@ -25,45 +22,56 @@ import ButtonDelete from '@/components/map/ButtonDelete.vue'
 import ButtonJoin from './ButtonJoin.vue'
 
 const gamesStore = useGamesStore()
-const props = defineProps(['id']);
+const props = defineProps(['id'])
+const showMarkers = ref(false)
 const matchId = props.id
 console.log('matchId' + matchId)
 
+function updateShowMarkers(value) {
+  showMarkers.value = value
+  console.log(showMarkers)
+}
 
 gamesStore.getMatch(matchId)
 
-const match = ref(computed(() => {
-  return gamesStore.oneMatch
-}))
- onMounted(()=> {
-  const startPoint = match.value.acf.start_point;
-    const markers = match.value.acf.markers;
-  
-      const latitude = startPoint.position.latitude;
-      const longitude = startPoint.position.longitude;
+const match = ref(
+  computed(() => {
+    return gamesStore.oneMatch
+  })
+)
+onMounted(() => {
+  const startPoint = match.value.acf.start_point
+  const markers = match.value.acf.markers
 
-      const map = L.map('map', {
-        center: [latitude, longitude],
-        zoom: 15,
-        layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
-      });
+  const latitude = startPoint.position.latitude
+  const longitude = startPoint.position.longitude
+
+  const map = L.map('map', {
+    center: [latitude, longitude],
+    zoom: 15,
+    layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')]
+  })
 
   map.whenReady(() => {
-    markers.forEach((marker) => {
-      const { latitude, longitude } = marker.position
-      const markerIcon = L.icon({
-        iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
-        iconSize: [50, 50],
-        iconAnchor: [12, 41],
-        popupAnchor: [0, -30]
-      })
-      const leaflermarker = L.marker([latitude, longitude], { icon: markerIcon })
-        .addTo(map)
-        .bindPopup(`<b>${marker.name}</b>`)
+    if (showMarkers.value) {
+      console.log('showMarkers est sur true')
+      markers.forEach((marker) => {
+        const { latitude, longitude } = marker.position
+        const markerIcon = L.icon({
+          iconUrl: 'https://www.svgrepo.com/show/374529/address.svg',
+          iconSize: [50, 50],
+          iconAnchor: [12, 41],
+          popupAnchor: [0, -30]
+        })
+        const leafletMarker = L.marker([latitude, longitude], { icon: markerIcon }).bindPopup(
+          `<b>${marker.name}</b>`
+        )
 
-      // Assurez-vous que le marqueur a une référence à leafletMarker
-      marker.leafletMarker = leaflermarker
-    })
+        marker.leafletMarker = leafletMarker.addTo(map)
+      })
+    } else {
+      console.log('showMarkers est sur false')
+    }
 
     const startPointIcon = L.icon({
       iconUrl: 'https://static.thenounproject.com/png/4418877-200.png',
@@ -79,7 +87,3 @@ const match = ref(computed(() => {
   })
 })
 </script>
-
-
-
-
