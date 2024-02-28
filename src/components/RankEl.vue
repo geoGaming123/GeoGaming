e003321578ad1fd4cb6ce94e483a378fb3d9b5ac
GiThibs <adelethibaut>
2024-02-28 10:03:30 +0100

    Edit rank name

--- src/components/RankEl.vue
+++ src/components/RankEl.vue
@@ -2,7 +2,7 @@
       <li class="rankList_El" :class="{ rankList_first: props.index === 0 }">
         <span>{{ props.index + 1}}</span>
         <img class="rankList_El_center" src="" alt=" ">
-        <span>monsuper.pseudo</span>
+        <span>{{ props.aPlayer.userId }}</span>
         <span class="rankList_El_right">{{ props.aPlayer.time }}</span>
       </li>
 </template>
