a384bae98fdfa62e1ee233772366229f3b867a98
GiThibs <adelethibaut@gmail.com>
2024-02-29 11:40:24 +0100

    Edit DashView

--- src/views/DashView.vue
+++ src/views/DashView.vue
@@ -2,7 +2,7 @@
   <main class="dashboard">
     <HeaderComp data="logo"></HeaderComp>
     <div class="dashboard-content">
-      <p>{{ myInfo.name }}</p>
+      <p>{{ myInfo }}</p>
       <div class="dashboard-content-circle"><img src="" alt="ProfilePic" /></div>
       <button class="dashboard-content-btn" @click="sendToHome">
         <img src="../assets/Icons/Logo-perso-blanc.png" alt="BTN" height="40px" width="40px" />
@@ -15,15 +15,13 @@
 import HeaderComp from '@/components/HeaderComp.vue'
 import { useRouter } from 'vue-router'
 import { useCounterStore } from '@/stores/counter'
-//import { useGamesStore } from '@/stores/games';
+import { useUserStore } from '@/stores/user';
 import { computed } from 'vue'
 
 const monStore = useCounterStore()
-//const gameStore = useGamesStore()
+const userStore = useUserStore()
 const router = useRouter()
-//const myID = computed(() => gameStore.sendUserId)
-monStore.getUser(10)
-const myInfo = computed(() => monStore.getMyUser)
+const myInfo = computed(() => userStore.userData.acf.pseudo)
 const sendToHome = () => {
   router.push(`/home`)
 }
