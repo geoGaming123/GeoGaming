634da32132dd05583f3a080dc97554d7d4f7e0b6
[Hazem Maddouri] <[hazemmaddourib@gmail.com]>
2024-03-01 14:24:36 +0100

    finish jsuppose

--- src/components/SignupComponent.vue
+++ src/components/SignupComponent.vue
@@ -11,12 +11,12 @@ const userStore = useUserStore()
     <form action="" method="" class="loginForm" @submit.prevent="userStore.createUser">
       <label for="email">Email :</label>
       <input v-model="userStore.userData.email" type="text" name="email" id="email" class="loginInput" placeholder="exemple@email.com">
-      <label for="password">Mot de passe :</label>
-      <input v-model="userStore.userData.password" type="password" name="password" id="password" class="loginInput" placeholder="******">
       <label for="pseudoName">Pseudo :</label>
       <input v-model="userStore.userData.username" type="text" name="pseudoName" id="pseudoName" class="loginInput" placeholder="Username">
+      <label for="password">Mot de passe :</label>
+      <input v-model="userStore.userData.password" type="password" name="password" id="password" class="loginInput" placeholder="******">
       <label for="avatarFile">Avatar :</label>
-      <input type="file" name="avatarFile" id="avatarFile" class="loginInput" />
+      <input @change="userStore.onFileUpload" type="file" name="avatarFile" id="avatarFile" class="loginInput" />
       <button class="loginBtn">Envoyer</button>
     </form>
     <a @click.prevent="userStore.pageBool = !userStore.pageBool" class="createAccount">Déjà un compte ?</a>
