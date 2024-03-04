7d3f06e4f3f397b01aa596331efb569964d93f77
[Hazem Maddouri] <[hazemmaddourib@gmail.com]>
2024-03-04 15:32:18 +0100

    userlogin small fix

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -4,15 +4,6 @@ import { ref } from 'vue';
 
 const userStore = useUserStore()
 
-//const username = ref('')
-//const password = ref('')
-
-/*const loginAction = () => {
-  userStore.setUserLogin(username.value, password.value)
-  username.value = ""
-  password.value = ""
-  userStore.loginUser()
-}*/
 </script>
 
 <template>
@@ -23,8 +14,8 @@ const userStore = useUserStore()
 
   <div class="card">
     <form action="" method="post" class="loginForm" @submit.prevent="userStore.loginUser">
-      <input v-model="userStore.userData.username" type="text" name="username" id="username" class="loginInput" placeholder="Pseudo">
-      <input v-model="userStore.userData.password" type="password" name="password" id="password" class="loginInput" placeholder="Mot de passe">
+      <input v-model="userStore.userLogin.username" type="text" name="username" id="username" class="loginInput" placeholder="Pseudo">
+      <input v-model="userStore.userLogin.password" type="password" name="password" id="password" class="loginInput" placeholder="Mot de passe">
       <button class="loginBtn">Connexion</button>
       <p class="login--separation"><span>Pas de compte ?</span></p>
       <button href="#" @click.prevent="userStore.pageBool = !userStore.pageBool" class="login--btn">Créer un compte</button>
